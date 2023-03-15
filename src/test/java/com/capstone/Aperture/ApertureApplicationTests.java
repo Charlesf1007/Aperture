898d928be4d7f026df2aee8a742f1d385102f3da
charles ferro <charlesf1007@gmail.com>
2023-03-15 09:24:55 +0000

    adding base layout of app

---
+++ src/test/java/com/capstone/Aperture/ApertureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.Aperture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApertureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
